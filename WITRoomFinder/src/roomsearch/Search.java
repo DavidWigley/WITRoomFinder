6cff8ce99c3345b460c09e9dac3db57b39cc0b2a
David Wigley <dwigley92@gmail.com>
2015-12-14 22:41:49 -0500

    Implemented basic searching based on time.

--- WITRoomFinder/src/roomsearch/Search.java
+++ WITRoomFinder/src/roomsearch/Search.java
@@ -71,19 +71,30 @@ public class Search {
 
 		for(int i = 0; i < list.size(); i++) {
 			String currentEntry = list.get(i).toString();
-			//think I gotta toss those CRN #'s
 			int hourBefore = currentHour - 1;
 			//deals with case where the current hour is 1
 			if (hourBefore == 0) {
 				hourBefore = 12;
 			}
 			int colonLocation = currentEntry.indexOf(":");
-			//this should return the first instance
+			//this should return the first instance of the colon
 			char[] firstTime = {currentEntry.charAt(colonLocation-2), currentEntry.charAt(colonLocation-1)};
-			int startHour = Character.getNumericValue(firstTime[0]) + Character.getNumericValue(firstTime[1]);
-			if (startHour < currentHour && startHour > currentHour - 2){
-				//potential problem
-				
+			//ok so this adds the numbers I actually want to just take on that num at the end
+			String time = Character.toString(firstTime[0]) + Character.toString(firstTime[1]);
+			int startHour = Integer.parseInt(time);
+			System.out.println("The start time was: " + startHour);
+			System.out.println("The current time is: " + currentHour);
+			if (startHour <= currentHour && startHour >= currentHour - 2){
+				//a class started around this time
+				char[] secondTime = {currentEntry.charAt(colonLocation+7), currentEntry.charAt(colonLocation+8)};
+				time = Character.toString(secondTime[0]) + Character.toString(secondTime[1]);
+				int endHour = Integer.parseInt(time);
+				System.out.println("The end time was: " + endHour);
+				if (endHour > currentHour){
+					//theres a class going on. It has not ended yet
+					list.remove(i);
+					i--;
+				}
 			}
 
 		}
