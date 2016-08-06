62cf8ad89982825902334e02142c90c094c5df13
David Wigley <dwigley92@gmail.com>
2016-08-06 19:07:32 -0400

    Adding a new class which will help make the occupation duration possible.
    Basically a nicer 2d arraylist.

---
+++ app/src/main/java/wigleyd/witroomfinder/TwoDimensionalArrayList.java
@@ -0,0 +1,32 @@
+package wigleyd.witroomfinder;
+
+import java.util.ArrayList;
+
+/**
+ * Created by david on 8/6/2016.
+ */
+
+class TwoDimentionalArrayList<T> extends ArrayList<ArrayList<T>> {
+
+    public void addToInnerArray(int index, T element) {
+        while (index >= this.size()) {
+            this.add(new ArrayList<T>());
+        }
+        this.get(index).add(element);
+    }
+
+    public void addToInnerArray(int index, int index2, T element) {
+        while (index >= this.size()) {
+            this.add(new ArrayList<T>());
+        }
+
+        ArrayList<T> inner = this.get(index);
+        while (index2 >= inner.size()) {
+            inner.add(null);
+        }
+
+        inner.set(index2, element);
+    }
+
+
+}
